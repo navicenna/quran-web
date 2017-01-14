9523bff32dcc4eb2af86666acb52c2041474d4e3
nbahmanyar <nbahmanyar@zoho.com>
2017-01-14 17:55:58 +0000

    up

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,93 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    # r = input("input")
-    return render_template("main_page.html")
+comments = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+    else:
+        com = request.form["contents"]
+        comments.append(com[0:2])
+        return redirect(url_for('index'))
+
+
 
 @app.route('/wibble')
 def wibble():
     return 'test'
+
+# Quran web analysis code
+
+# This Python file uses the following encoding: utf-8
+# Test commit
+
+import os, sys, re, csv
+
+arabic2english = {
+      u"\u0621": "'", # hamza-on-the-line
+      u"\u0622": "|", # madda
+      u"\u0623": ">", # hamza-on-'alif
+      u"\u0624": "&", # hamza-on-waaw, treat as waaw for now
+      u"\u0625": "<", # hamza-under-'alif
+      u"\u0626": "}", # hamza-on-yaa'
+      u"\u0627": "A", # bare 'alif
+      u"\u0628": "b", # baa'
+      u"\u0629": "h", # taa' marbuuTa, treat as haa'
+      u"\u062A": "t", # taa'
+      u"\u062B": "v", # thaa'
+      u"\u062C": "j", # jiim
+      u"\u062D": "H", # Haa'
+      u"\u062E": "x", # khaa'
+      u"\u062F": "d", # daal
+      u"\u0630": "*", # dhaal
+      u"\u0631": "r", # raa'
+      u"\u0632": "z", # zaay
+      u"\u0633": "s", # siin
+      u"\u0634": "$", # shiin
+      u"\u0635": "S", # Saad
+      u"\u0636": "D", # Daad
+      u"\u0637": "T", # Taa'
+      u"\u0638": "Z", # Zaa' (DHaa')
+      u"\u0639": "E", # cayn
+      u"\u063A": "g", # ghayn
+      u"\u0640": "_", # taTwiil
+      u"\u0641": "f", # faa'
+      u"\u0642": "q", # qaaf
+      u"\u0643": "k", # kaaf
+      u"\u0644": "l", # laam
+      u"\u0645": "m", # miim
+      u"\u0646": "n", # nuun
+      u"\u0647": "h", # haa'
+      u"\u0648": "w", # waaw
+      u"\u0649": "y", # 'alif maqSuura, replace with yaa'
+      u"\u064A": "y", # yaa'
+      u"\u064B": "F", # fatHatayn
+      u"\u064C": "N", # Dammatayn
+      u"\u064D": "K", # kasratayn
+      u"\u064E": "a", # fatHa
+      u"\u064F": "u", # Damma
+      u"\u0650": "i", # kasra
+      u"\u0651": "~", # shaddah
+      u"\u0652": "o", # sukuun
+      u"\u0670": "`", # dagger 'alif
+      u"\u0671": "{", # waSla
+}
+
+def transString(string, reverse=0):
+    '''Given a Unicode string, transliterate into Buckwalter. To go from
+    Buckwalter back to Unicode, set reverse=1'''
+    silents = ["|", "&", "F", "N", "K", "a", "u", "i", "~", "o", "{"] #map these diacritical marks to empty string
+
+    string = string.replace("|", ".")
+    for k,v in arabic2english.items():
+        string = string.replace(k,v)
+    for letter in silents:
+        string = string.replace(letter, "")
+
+    return string
