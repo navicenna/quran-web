3919e507e64603fbe619beb754684333045e968f
nbahmanyar <nbahmanyar@zoho.com>
2017-11-18 00:59:48 +0000

    updated Quran search to all suras

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, Markup
 import logging
 import re
 from flask_debugtoolbar import DebugToolbarExtension
@@ -51,16 +51,14 @@ class Verse(db.Model):
 
 # Variables to determine whether the raw Quran input should be loaded upon running
 #   the script, and if it should be loaded, just a test portion or the whole Quran
-quran_load = False
+quran_load = True
 quran_test = False
 
 # Load Quran into table. First delete it, then add rows.
-
+quran_file = "/home/navid/mysite/eng_quran_out.txt"
 if quran_load:
     n = Verse.query.delete()
     db.session.commit()
-
-    quran_file = "/home/navid/mysite/eng_quran_out.txt"
 
     testlines=[]
     with open(quran_file, "r", encoding="utf-8") as f:
@@ -89,19 +87,26 @@ if quran_load:
         db.session.add(temp_verse)
     db.session.commit()
 
-
+# Quran dictionary
+quran_dict = scrape_quran_into_dict(quran_file)
 
 # Store queried verses
 verse_obj = []
+# Store alif count
+alif_count = "N/A"
+# other vars
+result = ""
+
 
 # Main web application function
 @app.route("/", methods=["GET", "POST"])
 def index():
-    global verse_obj
+    global verse_obj, alif_count
 
     # test=['a', 'b']
     if request.method == "GET":
-        return render_template("main_page.html", comments=Word.query.all(), verses=verse_obj)
+        return render_template("main_page.html", comments=Word.query.all(), verses=verse_obj,
+                                 alif_count=alif_count)
     else:
         execute_this = request.form["submit"]
         logging.error(request.form)
@@ -116,6 +121,22 @@ def index():
             comment = Word(content=string)
             db.session.add(comment)
             db.session.commit()
+
+        elif execute_this == "Count alif":
+            req = request.form["contents"].strip()
+            req = list( map( int, re.split("\D+", req) ))
+
+            if len(req) == 1:
+                sura = req[0]
+                alif_count = alif_count_sura(quran_dict, sura, ["A"])
+            else:
+                try:
+                    sura, verse = req[0], req[1]
+                    alif_count = alif_count_verse(quran_dict, sura, verse, ["A"])
+                except:
+                    alif_count = "Invalid sura-verse input"
+
+
         elif execute_this == "Get verse":
             req = request.form["contents"].strip()
             if re.match("\d+\D+\d+\D*",req):
@@ -126,14 +147,41 @@ def index():
 
 
 
-@app.route('/wibble')
-def wibble():
-    return 'test'
+# @app.route('/wibble')
+# def wibble():
+#     return 'test'
 
 
 @app.route('/example_arabic_lesson')
-def display():
+def example_arabic_lesson():
     return render_template("example_arabic_lesson.html")
+
+# Letter count dashboard
+@app.route('/dashboard', methods=["GET", "POST"])
+def dashboard():
+    global result
+
+    # result = "test result"
+    if request.method == "POST":
+        letters_selected = request.form.getlist('letter')
+        print(letters_selected)
+        sura_values = alif_count_quran(quran_dict, letters_selected)
+        letter_count = sura_values["count"]
+        tgv = sura_values["tgv"]
+        result = Markup("Count of selected letters in Quran: " + str(letter_count) +
+                    "<br>Total TGV of selected letters in Quran: " + str(tgv))
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template("dashboard.html", letters=fetch_trans_dict(), result=result)
+
+
+
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#       HELPER FUNCTIONS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
@@ -210,7 +258,8 @@ def count_letter(verse, letter):
 
 
 
-
+if __name__ == "main":
+    app.debug = True
 
 
 
--- funcs_process_quran_text.py
+++ funcs_process_quran_text.py
@@ -2,7 +2,8 @@
 
 import re
 from pprint import pprint
-
+from flask import Markup
+from funcs import calc_val
 
 # A dictionary that maps every possible Arabic letter to an English transliteration
 arabic2english = {
@@ -60,7 +61,7 @@ arabic2english = {
 def transString(string):
     '''Given a Unicode string, transliterate into Buckwalter'''
 
-    silents = ["|", "F", "N", "K", "a", "u", "i", "~", "o", "{"] #map these diacritical marks to empty string
+    silents = ["|", "F", "N", "K", "a", "u", "i", "~", "o"] #map these diacritical marks to empty string. Note, waslah was just removed from here (11/17/2017)
 
     # string = string.replace("|", ".")
     for k,v in arabic2english.items():
@@ -110,7 +111,7 @@ def scrape_verse(quran_dict, verse_text):
 
 # Open the text file of Quran and scrape it into a nested dictionary
 # data structure
-# 
+#
 # Returns: dictionary
 def scrape_quran_into_dict(quran_file):
   with open(quran_file, "r", encoding="utf-8-sig") as f:
@@ -124,34 +125,151 @@ def scrape_quran_into_dict(quran_file):
 
 
 # alif_count_verse: given Quran dictionary and a sura and verse number,
-#                   count the number of alifs in the verse
-# 
+#                   count the number of alifs in the verse, based on certain
+#                   assumptions about what does and doesn't constitute an alif
+#
 # Returns: integer
-def alif_count_verse(quran_dict, sura, verse):
-  trans = transString(quran_dict[sura][verse]["arabic"])
-  print(sura, verse, trans)
-  return sum( [1 for letter in trans if letter=="A"] )
+def alif_count_verse(quran_dict, sura, verse, assumptions):
+  mod = "strong"
+  try:
+    trans = transString(quran_dict[sura][verse]["arabic"])
+    print(sura, verse, trans)
+    total = sum( [1 for letter in trans if letter in assumptions] )
+    letters = "".join([letter for letter in trans if letter in assumptions])
+    return {"verse": Markup(quran_dict[sura][verse]["arabic"].replace("ا",  "<" + mod + ">ا</" + mod + ">"   )),
+            "count": total,
+            "tgv":   calc_val(letters, "tgv")}
+  except:
+     {"verse": Markup("<b>Error</b> in function 'alif_count_sura': Invalid sura and verse number "),
+            "count": "Error in alif_count_verse function"}
+
 
 
 # alif_count_sura: given Quran dictionary and a sura number,
 #                   count the number of alifs in the sura
-# 
+#
 # Returns: integer
-def alif_count_sura(quran_dict, sura):
-  total = 0
-  # pprint( quran_dict[sura].keys())
-  for verse in quran_dict[sura].keys():
-    total += alif_count_verse(quran_dict, sura, verse)
-  return total
+def alif_count_sura(quran_dict, sura, assumptions):
+    total, tgv = 0, 0
+    # pprint( quran_dict[sura].keys())
+    try:
+        for verse in quran_dict[sura].keys():
+            verse_values = alif_count_verse(quran_dict, sura, verse, assumptions)
+            total += verse_values["count"]
+            tgv += verse_values["tgv"]
+        # rv = total
+        rv = {"verse": Markup("Alif count in sura " + str(sura) + " = <b>" + str(total) + "</b>"),
+              "count": total,
+              "tgv":   tgv}
+    except:
+        rv = {"verse": Markup("<b>Error</b> in function 'alif_count_sura'"),
+              "count": "Error in alif_count_sura function"}
+
+    return rv
+
+
+
+# alif_count_sura: given Quran dictionary and a sura number,
+#                   count the number of alifs in the sura
+#
+# Returns: integer
+def alif_count_quran(quran_dict, assumptions):
+    total, tgv = 0, 0
+    # pprint( quran_dict[sura].keys())
+    try:
+        for sura in quran_dict.keys():
+            for verse in quran_dict[sura].keys():
+                verse_values = alif_count_verse(quran_dict, sura, verse, assumptions)
+                total += verse_values["count"]
+                tgv += verse_values["tgv"]
+            # rv = total
+        rv = {"verse": Markup("Alif count in sura " + str(sura) + " = <b>" + str(total) + "</b>"),
+              "count": total,
+              "tgv":   tgv}
+    except:
+        rv = {"verse": Markup("<b>Error</b> in function 'alif_count_quran'"),
+              "count": "Error in alif_count_sura function"}
+
+    return rv
+
+
+
+# Fetch and return the Arabic transliteration dictionary
+def fetch_trans_dict():
+    letters = [
+      [
+      (u"\u0627", "A"), # bare 'alif
+      (u"\u0670", "`"), # dagger 'alif
+      (u"\u0671", "{"), # waSla
+      (u"\u0621", "'"), # hamza-on-the-line
+      (u"\u0622", "|") # madda
+      ],
+      [
+      (u"\u0623", "A"), # hamza-on-'alif
+      (u"\u0624", "&"), # hamza-on-waaw, treat as waaw for now
+      (u"\u0625", "<"), # hamza-under-'alif
+      (u"\u0626", "}") # hamza-on-yaa'
+      ],
+      [
+      (u"\u0628", "b"), # baa'
+      (u"\u0629", "h"), # taa' marbuuTa, treat as haa'
+      (u"\u062A", "t"), # taa'
+      (u"\u062B", "v") # thaa'
+      ],
+      [
+      (u"\u062C", "j"), # jiim
+      (u"\u062D", "H"), # Haa'
+      (u"\u062E", "x"), # khaa'
+      ],
+      [
+      (u"\u062F", "d"), # daal
+      (u"\u0630", "*"), # dhaal
+      (u"\u0631", "r"), # raa'
+      (u"\u0632", "z"), # zaay
+      ],
+      [
+      (u"\u0633", "s"), # siin
+      (u"\u0634", "$"), # shiin
+      (u"\u0635", "S"), # Saad
+      (u"\u0636", "D") # Daad
+      ],
+      [
+      (u"\u0637", "T"), # Taa'
+      (u"\u0638", "Z"), # Zaa' (DHaa')
+      (u"\u0639", "E"), # cayn
+      (u"\u063A", "g") # ghayn
+      ],
+      [
+      (u"\u0641", "f"), # faa'
+      (u"\u0642", "q"), # qaaf
+      (u"\u0643", "k"), # kaaf
+      (u"\u0644", "l") # laam
+      ],
+      [
+      (u"\u0645", "m"), # miim
+      (u"\u0646", "n"), # nuun
+      (u"\u0648", "w"), # waaw
+      ],
+      [
+      (u"\u0647", "h"), # haa'
+      (u"\u0649", "y"), # 'alif maqSuura, replace with yaa'
+      (u"\u064A", "y") # yaa'
+      ],
+    ]
+    return letters
+
+
+
 
 # TESTING
-f = "eng_quran_out.txt"
-d = scrape_quran_into_dict(f)
-# print(d[1])
-# pprint.pprint(d)
+if __name__ == "__main__":
+    f = "eng_quran_out.txt"
+    # d = scrape_quran_into_dict(f)
+    # print(d[1])
+    # pprint.pprint(d)
 
 
-# c = alif_count_verse(d, 1, 1)
-total = alif_count_sura(d, 1)
-print(total)
-# pprint(d[1])
+    # c = alif_count_verse(d, 1, 1)
+    # total = alif_count_sura(d, 1)
+    # print(total)
+    # pprint(d[1])
